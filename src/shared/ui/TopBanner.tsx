931a0dd96a8cbd721ef2ae12cec8fb4e897bee13
ChristianEducation <c.wevarh@gmail.com>
2026-06-14 22:56:17 -0400

    style: ocultar banner superior por solicitud de cliente

--- src/shared/ui/TopBanner.tsx
+++ src/shared/ui/TopBanner.tsx
@@ -1,11 +1,3 @@
-import { TopBannerClient } from "./TopBannerClient";
-
 export async function TopBanner() {
-  return (
-    <TopBannerClient
-      description={null}
-      linkUrl={null}
-      title="🚚 Envíos a todo Chile · Retiro gratis en Antofagasta"
-    />
-  );
+  return null;
 }
